ac8a40d08aed7e528ecc2d0c3efbfaa5d4e84235
Jai Vardhan <pdmnvardhan510@gmail.com>
2025-08-13 09:12:49 -0500

    added backend route for map

---
+++ api/map.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+
+const KEY = process.env.GMAPS_KEY;
+
+router.route("/gyms/:lat/:lng/:radius").get(async (req, res) => {
+  const { lat, lng, radius } = req.params;
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=gym&key=${process.env.GMAPS_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  res.send(data);
+});
+
+export default router;
