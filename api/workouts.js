18cbaa831153db3e5a166642cc6697c0c4978987
karl <karl.fahlbusch7241@gmail.com>
2025-08-15 10:48:56 -0500

    make smalladjustment to workout db and api to call the to front better

--- api/workouts.js
+++ api/workouts.js
@@ -1,11 +1,27 @@
+// api/workouts.js
 import express from "express";
+import { getAllWorkouts, getWorkoutById } from "#db/queries/workouts";
+
 const router = express.Router();
-export default router;
 
-import { getAllWorkouts } from "#db/queries/workouts";
-
-router.route("/").get(async (req, res) => {
-    workouts = getAllWorkouts();
-    if (!workouts) return res.status(404).send("no workouts found");
+router.get("/", async (req, res, next) => {
+  try {
+    const workouts = await getAllWorkouts();
+    if (!workouts.length) return res.status(404).send("no workouts found");
     res.send(workouts);
-})
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const w = await getWorkoutById(req.params.id);
+    if (!w) return res.status(404).send("workout not found");
+    res.send(w);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
